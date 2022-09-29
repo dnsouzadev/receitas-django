71f12b58ad2207f34a5b4f7cdd3d012249ff8497
Daniel Souza <workdndsza@gmail.com>
2022-09-28 22:28:40 -0300

    exemplo de template

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'buerin'
+    })
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
