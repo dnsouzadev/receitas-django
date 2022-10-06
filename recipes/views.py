faa6dcd4dfc7063bce7eaec3ea3ba1bd662ee4ce
daniel souza <workdndsza@gmail.com>
2022-10-06 19:53:56 -0300

    criando pagina unica da recipe

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'buerin'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': id
+    })
