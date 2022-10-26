d81fdda400d85b407a67a9e3a3181869ca3971ee
daniel souza <workdndsza@gmail.com>
2022-10-25 22:00:08 -0300

    criando testes pra category model

---
+++ recipes/tests/test_recipe_model_categ.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeCategoryModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.category = self.make_category(
+            name='Category Test'
+        )
+        return super().setUp()
+
+    def test_recipe_category_model_string_representation_is_name_field(self):
+        self.assertEqual(
+            str(self.category),
+            self.category.name,
+            msg='Should be equal category name'
+        )
+
+    def test_recipe_category_model_name_max_length(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
