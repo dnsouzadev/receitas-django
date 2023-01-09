25f1a5212ca8f16dec305064c7813fa975da17ec
Daniel Souza <workdndsza@gmail.com>
2023-01-09 13:21:19 -0300

    autenticando usuario com authenticate e login

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 
@@ -14,7 +15,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:create')
+        'form_action': reverse('authors:register_create')
     })
 
 
@@ -35,3 +36,35 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login_view.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid Credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
