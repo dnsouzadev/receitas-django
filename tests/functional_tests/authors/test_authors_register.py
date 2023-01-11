f40677f07ca25b48184bdfe135311affdb9e6303
Daniel Souza <workdndsza@gmail.com>
2023-01-11 13:28:08 -0300

    testando a mensagem de erro do last name

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -17,23 +17,44 @@ class AuthorsRegisterTest(AuthorsBaseTest):
             if field.is_displayed():
                 field.send_keys(' ' * 20)
 
-    def test_empty_first_name_error_message(self):
-        self.browser.get(self.live_server_url + '/authors/register/')
-        form = self.browser.find_element(
+    def get_form(self):
+        return self.browser.find_element(
             By.XPATH,
             '/html/body/main/div[2]/form'
         )
+
+    def form_field_test_with_callback(self, callback):
+        self.browser.get(self.live_server_url + '/authors/register/')
+        form = self.get_form()
 
         self.fill_form_dummy_data(form)
         form.find_element(By.NAME, 'email').send_keys('dummy@email.com')
 
-        first_name_field = self.get_by_placeholder(form, 'Ex.: John')
-        first_name_field.send_keys(' ')
-        first_name_field.send_keys(Keys.ENTER)
+        callback(form)
+        return form
 
-        form = self.browser.find_element(
-            By.XPATH,
-            '/html/body/main/div[2]/form'
-        )
+    def test_empty_first_name_error_message(self):
 
-        self.assertIn('Write your first name', form.text)
+        def callback(form):
+            first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+            first_name_field.send_keys(' ')
+            first_name_field.send_keys(Keys.ENTER)
+
+            form = self.get_form()
+
+            self.assertIn('Write your first name', form.text)
+
+        self.form_field_test_with_callback(callback)
+
+    def test_empty_last_name_error_message(self):
+
+        def callback(form):
+            last_name_field = self.get_by_placeholder(form, 'Ex.: Doe')
+            last_name_field.send_keys(' ')
+            last_name_field.send_keys(Keys.ENTER)
+
+            form = self.get_form()
+
+            self.assertIn('Write your last name', form.text)
+
+        self.form_field_test_with_callback(callback)
