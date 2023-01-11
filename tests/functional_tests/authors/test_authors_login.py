a43300c108be50a0afd0547035140f4f7e36fa3e
Daniel Souza <workdndsza@gmail.com>
2023-01-11 14:42:23 -0300

    testando o formulario para credenciais invalidas

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -58,3 +58,23 @@ class AuthorsLoginTest(AuthorsBaseTest):
             'Invalid username or password',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_form_login_invalid_credentials(self):
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('invalid_user')
+        password_field.send_keys('invalid_password')
+
+        form.submit()
+
+        self.assertIn(
+            'Invalid Credentials',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
