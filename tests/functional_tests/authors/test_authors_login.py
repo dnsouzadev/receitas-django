4fd365461a7552676b063dc32104b43ef0867f38
Daniel Souza <workdndsza@gmail.com>
2023-01-11 14:37:37 -0300

    testando se o formulario de login e valido

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -38,3 +38,23 @@ class AuthorsLoginTest(AuthorsBaseTest):
             'Not Found',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_form_login_is_invalid(self):
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('   ')
+        password_field.send_keys('   ')
+
+        form.submit()
+
+        self.assertIn(
+            'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
