6f7897808fc14f2d0ac1d6252af3af5fac0509fe
Daniel Souza <workdndsza@gmail.com>
2023-01-13 10:59:58 -0300

    corrigindo o grid e css

--- utils/django_forms.py
+++ utils/django_forms.py
@@ -7,6 +7,11 @@ def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
 def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
