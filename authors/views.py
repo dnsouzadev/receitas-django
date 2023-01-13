5e301e451a4ba542215ca6fe28d9dbfae395c6ae
Daniel Souza <workdndsza@gmail.com>
2023-01-13 10:49:12 -0300

    creating a form for add recipes or editing

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from authors.forms.recipe_form import AuthorRecipeForm
 from recipes.models import Recipe
 
 from .forms import LoginForm, RegisterForm
@@ -106,11 +107,16 @@ def dashboard_recipe_edit(request, id):
         is_published=False,
         author=request.user,
         pk=id
-    )
+    ).first()
 
     if not recipe:
         raise Http404()
 
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
+
     return render(request, 'authors/pages/dashboard_recipe.html', {
-        'recipe': recipe
+        'form': form
     })
